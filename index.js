ad9a9d5dcb448125e145660b5d2657867a075318
Hrithik Biswas <hrithikpersonalid@gmail.com>
2021-07-30 20:51:20 +0600

    added new data

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const myFucntion = (msg, presentYear, birthYear) => {
-    let name = "Hrithik";
+    let name = "Sonchita";
     let age = presentYear - birthYear;
-    return `${msg} ${name}, You are ${age} years old.`;
+    let obj = {
+        name: "Hrithik",
+        title: "Biswas",
+        nationality: "Bangladeshi",
+        age: 21,
+    };
+    let print = `${obj.name} ${obj.title} is a ${obj.nationality} person. He is ${obj.age} years old.`;
+    return `${msg} ${name}, You are ${age} years old. \n${print}`;
 };
 
-const output = myFucntion("Hello", 2021, 2000); // Hello Hrithik, You are 21 years old.
+const output = myFucntion("Hello", 2021, 2018); // Hello Hrithik, You are 21 years old.
 
 console.log(output);
