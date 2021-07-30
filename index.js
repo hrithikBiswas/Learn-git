9daab096a1a37a0eac47d60b3023d6032d4c6e7f
Hrithik Biswas <hrithikpersonalid@gmail.com>
2021-07-30 22:25:03 +0600

    create test3 branch & made some changes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const myFucntion = (msg, age) => {
-    let name = "sonchita";
+    let name = "sonchita Biswas";
     let date = new Date();
     let year = date.getFullYear() - age;
     return `${msg} ${name}, You are born in ${year}.`;
 };
 
-const output = myFucntion("Hi", 18); // Hi Sonchita, You are born in 18.
+const output = myFucntion("Hi", 18); // Hi Sonchita Biswas, You are born in 18.
 
 console.log(output);
