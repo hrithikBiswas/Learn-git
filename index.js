08277c5942eee912b95d56795858715ec8e89857
Hrithik Biswas <hrithikpersonalid@gmail.com>
2021-07-30 17:50:09 +0600

    updated index.js file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const myFucntion = (msg) => {
+const myFucntion = (msg, presentYear, birthYear) => {
     let name = "Hrithik";
-    return `${msg} ${name}`;
+    let age = presentYear - birthYear;
+    return `${msg} ${name}, You are ${age} years old.`;
 };
 
-const output = myFucntion("Hello");
+const output = myFucntion("Hello", 2021, 2000); // Hello Hrithik, You are 21 years old.
 
 console.log(output);
