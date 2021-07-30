68d29dd27fa2359f9cca89afab045e08d1e061ab
Hrithik Biswas <hrithikpersonalid@gmail.com>
2021-07-30 20:32:19 +0600

    Some changes information in index.js file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const myFucntion = (msg, presentYear, birthYear) => {
-    let name = "Hrithik";
+    let name = "sonchita";
     let age = presentYear - birthYear;
     return `${msg} ${name}, You are ${age} years old.`;
 };
 
-const output = myFucntion("Hello", 2021, 2000); // Hello Hrithik, You are 21 years old.
+const output = myFucntion("Welcome", 2021, 2003); // Hello Hrithik, You are 21 years old.
 
 console.log(output);
